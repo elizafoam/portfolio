a30742c1d16a2adfc435d949a565f96ac4077a24
Elizabeth J <elizabeth@Elizabeths-MacBook-Air.local>
2024-12-27 19:18:07 -0500

    add icon navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,9 +10,9 @@ const Header = () => {
       </div>
       <div className='name manrope-body'>my name is Elizabeth J.</div>
       <div className='socials-icons'>
-        <img className='icon' src="github.png" alt="github icon" />
-        <img className='icon' src="linkedin.png" alt="linkedin icon" />
-        <img className='icon' src="gmail.png" alt="gmail icon" />
+      <a href="https://github.com/elizafoam"><img className='icon' src="github.png" alt="github icon" /></a>
+      <a href="https://www.linkedin.com/in/elizafoam/"><img className='icon' src="linkedin.png" alt="linkedin icon" /></a>
+      <a href="mailto:elizafoam@gmail.com"><img className='icon' src="gmail.png" alt="gmail icon" /></a>
       </div>
     </div>
   )
